bb618e7ad1f410438ad6b49c2e85b0d8ba510eb5
diksset <diksset@gmail.com>
2020-02-08 19:52:34 +0300

    Fixed some syntax

--- config.py
+++ config.py
@@ -1,4 +1,2 @@
-TOKEN = '1044400892:AAE8s7L6PYfA_XxjAcPBLSJ6xShzwn1smzM'
+﻿TOKEN = '1044400892:AAE8s7L6PYfA_XxjAcPBLSJ6xShzwn1smzM'
 AdminInfo = ['650332908'] #список id админов
-#'529797809' - романов
-#'342867134' - данилов
